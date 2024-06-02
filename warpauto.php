f4bacfec6149789109ab5dd1860a42b284de93bf
ÐΛɌ₭ᑎΞ𐒡𐒡 <104245967+mansor427@users.noreply.github.com>
2024-06-02 14:20:19 +0330

    Update warpauto.php

--- warpauto.php
+++ warpauto.php
@@ -7,8 +7,7 @@
     $warp .= "//profile-update-interval: 24\n";
     $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
     $warp .= "//profile-web-page-url: https://github.com/mansor427\n\n";
-    $warp .= "warp://auto#@mansor427🇮🇷 &&detour=warp://auto#ÐΛɌ₭ᑎΞ𐒡𐒡🇩🇪";
-
+    $warp .= "warp://auto#@mansor427🇮🇷 &&detour=warp://auto#ÐΛɌ₭ᑎΞ𐒡𐒡🇩🇪"\n;
     $warp .= "warp://@auto/?ifp=5-10#🇮🇷@mansor427-IR &&detour=warp://@auto/?ifp=5-10#🇩🇪 ÐΛɌ₭ᑎΞ𐒡𐒡-DE";
 
    $i = 1;
